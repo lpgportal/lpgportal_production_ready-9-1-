446b89666743626c2b22f252d13598c4b25177f6
lpgportal <kerem.kar@outlook.com.tr>
2026-08-01 16:34:50 +0300

    Implement 301 redirect from www to non-www
    Add middleware to redirect www to non-www for the site.

--- server.ts
+++ server.ts
@@ -22,6 +22,15 @@ dotenv.config();
 const app = express();
 const PORT = 3000;
 
+// 301 Redirect www to non-www
+app.use((req, res, next) => {
+  const host = req.headers.host;
+  if (host && host.startsWith("www.lpgportal.com")) {
+    return res.redirect(301, `https://lpgportal.com${req.originalUrl}`);
+  }
+  next();
+});
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
